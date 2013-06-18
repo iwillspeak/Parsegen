54a4afec3dde02381e36537088cf90569748de30
Will Speak <lithiumflame@gmail.com>
2013-06-18 15:21:47 +0100

    Fix File Modes

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup
 setup(
     name = 'parsegen',
     packages = ['test', 'parsegen'],
+	scripts = ['bin/parsegen'],
     version = '0.0.1',
     description = 'LL(1) Parser Generator',
     author = 'Will Speak (@willspeak)',
