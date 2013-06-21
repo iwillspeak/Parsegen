fd0763c32720a657399b554dc1460e02a570c583
Will Speak <lithiumflame@gmail.com>
2013-06-21 14:26:46 +0100

    Reorder Setup Script

--- setup.py
+++ setup.py
@@ -1,18 +1,28 @@
 from distutils.core import setup
 import parsegen.version
 setup(
-    name = 'parsegen',
-    packages = ['test', 'parsegen'],
-	scripts = ['bin/parsegen'],
-    version = parsegen.version.VERSION,
-    description = 'LL(1) Parser Generator',
-    author = 'Will Speak (@willspeak)',
-    author_email = 'parsegen@willspeak.me',
-    url = 'http://github.com/iwillspeak/Parsegen',
-    classifiers = [
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
-    ]
+	# Metadata
+	name = 'parsegen',
+	version = parsegen.version.VERSION,
+	author = 'Will Speak (@willspeak)',
+	author_email = 'parsegen@willspeak.me',
+	maintainer = 'Will Speak',
+	maintainer_email = 'parsegen@willspeak.me',
+	url = 'http://github.com/iwillspeak/Parsegen',
+	description = 'LL(1) Parser Generator',
+	long_description = """
+	Parsegen is an automatic parser generator for C. It reads in definitions in
+	a dialect of BNF and creates a top-down parser with one-symbol lookahead.
+
+	In theory Parsegen can generate parsers for LL(1) grammars.
+	""",
+	classifiers = [
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent"
+	],
+	# Things to Distribute
+	packages = ['test', 'parsegen'],
+	scripts = ['bin/parsegen']
 )
